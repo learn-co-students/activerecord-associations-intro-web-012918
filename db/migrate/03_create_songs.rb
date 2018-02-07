d2064270fd7c4a6962c142a145f008c21fe47346
brookescarlett <brooke.yalof2017@u.northwestern.edu>
2018-02-07 17:55:09 -0500

    done

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,16 @@
 class CreateSongs < ActiveRecord::Migration
+
+  def up
+  end
+  
+  def down
+  end
+
+  def change
+    create_table :songs do |t|
+      t.string :name
+      t.integer :artist_id
+      t.integer :genre_id
+    end
+  end
 end
