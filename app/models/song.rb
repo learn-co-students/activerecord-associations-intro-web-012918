a66ac83a9546dd184eb2844e48b033fdb530ff7d
github username <github email address>
2018-02-09 12:11:12 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,7 @@
 class Song < ActiveRecord::Base
+  belongs_to :artist
+  belongs_to :genre
+  #refers to the artist and genre tables but the method takes in the singular version of the table name
+  #because that just how the macro methods work....meh
 
 end
